d433c6b9971a0317ddbfcb20aca7975e231b2823
JeanMainguy <jean.mainguy@outlook.fr>
2025-04-29 16:27:48 +0200

    update test with genome_metadata_sources

--- tests/manage_db/test_genome_in_pangenome.py
+++ tests/manage_db/test_genome_in_pangenome.py
@@ -1,8 +1,6 @@
 from typing import Any, Dict
 from pangbank_api.models import (
     Genome,
-    GenomeInPangenomeMetadata,
-    GenomeInPangenomeMetadataSource,
     GenomePangenomeLink,
     Pangenome,
 )
@@ -15,43 +13,6 @@ from ..mock_data import (
     pangenome_metric_data,  # type: ignore # noqa: F401 # pylint: disable=unused-import
     genome_in_pangenome_metric_data,  # type: ignore # noqa: F401 # pylint: disable=unused-import
 )
-
-
-def test_add_genome_in_pangenome_metadata(session: Session, mock_data: None):
-    """Test with no filters applied, should return all pangenomes."""
-
-    genome_pangenome_link = session.exec(select(GenomePangenomeLink)).first()
-
-    if genome_pangenome_link is None:
-        pytest.fail("No GenomePangenomeLink found in the database")
-
-    metadata_source = GenomeInPangenomeMetadataSource(name="DB_A")
-
-    metadata = GenomeInPangenomeMetadata(
-        key="Feature1",
-        value="1",
-        source=metadata_source,
-        genome_in_pangenome=genome_pangenome_link,
-    )
-
-    session.add(metadata_source)
-    session.add(metadata)
-    session.commit()
-
-    metadata = session.exec(select(GenomeInPangenomeMetadata)).first()
-    assert metadata is not None
-    assert metadata.key == "Feature1"
-    assert metadata.value == "1"
-    assert metadata.source.name == "DB_A"
-    assert metadata.genome_in_pangenome.genome_id is not None
-    assert metadata.genome_in_pangenome.genome_id == genome_pangenome_link.genome_id
-    assert metadata.genome_pangenome_link_id == genome_pangenome_link.id
-
-    session.delete(genome_pangenome_link)
-    session.commit()
-
-    metadata = session.exec(select(GenomeInPangenomeMetadata)).first()
-    assert metadata is None
 
 
 def test_genome_in_pangenome_uniqness(
